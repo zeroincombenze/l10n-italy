09e19f94722d2a43ea35423748f30c83d80a4d29
zeroincombenze <info@shs-av.com>
2018-10-22 13:21:04 +0200

    [IMP] l10n_it_* for VAT Communication & eInvoice

--- l10n_it_pec/models/partner.py
+++ l10n_it_pec/models/partner.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2014    Associazione Odoo Italia
-# Copyright 2018-19 - Odoo Italia Associazione <https://www.odoo-italia.org>
+# Copyright 2014    Associazione Odoo Italia (<https://www.odoo-italia.org>)
 # Copyright 2018-19 - SHS-AV s.r.l. <https://www.zeroincombenze.it>
 #
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 #
-#    (<http://www.openerp-italia.org>).
-#
-
 from openerp import fields, models
 
 
