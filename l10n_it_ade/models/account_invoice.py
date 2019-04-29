4aad2bc706c1d954077c2b617e0159fff6f22ba6
zeroincombenze <info@shs-av.com>
2019-04-29 12:13:36 +0200

    [IMP] VAT communication upgrade

---
+++ l10n_it_ade/models/account_invoice.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018-19 - SHS-AV s.r.l. <https://www.zeroincombenze.it>
+#
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+#
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
+
+
+XML_ESCAPE = {
+    u'\'': u' ',
+    u'\n': u' ',
+    u'\r': u' ',
+    u'\t': u' ',
+    u'€': u'EUR',
+    u'©': u'(C)',
+    u'®': u'(R)',
+    u'«': u'&laquo;',
+    u'»': u'&raquo;',
+    u'Ø': u'&Oslash;',
+    u'ø': u'&oslash;',
+    u'ß': u'ss',
+}
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    def wep_text(self, text):
+        """"Do xml escape to avoid error StringLatinType"""
+        if text:
+            return escape(unidecode(text), XML_ESCAPE).strip()
+        return text
