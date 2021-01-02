9c4f44f4f49c977f1b76684e1816dc95d495607c
Antonio Maria Vigliotti <antoniomaria.vigliotti@gmail.com>
2021-01-02 20:56:15 +0100

    [FIX] l10n_it_ddt: infinire recurse when cancel sale order

--- l10n_it_ddt/models/stock.py
+++ l10n_it_ddt/models/stock.py
@@ -10,17 +10,17 @@ from odoo import models, api
 class StockPicking(models.Model):
     _inherit = 'stock.move'
 
-    @api.multi
-    def action_cancel(self):
-        for move in self:
-            if move.procurement_id:
-                move.procurement_id.cancel()
-            for quant in move.quant_ids:
-                quant.quants_move(
-                    quant, move, move.location_id,
-                    location_from=move.location_dest_id,
-                    lot_id=move.lot_ids and move.lot_ids[0] or False)
-            move.write({'state': 'cancel', 'date': False})
-            if move.picking_id:
-                move.picking_id.action_revert_done()
-        return True
+    # @api.multi
+    # def action_cancel(self):
+    #     for move in self:
+    #         if move.procurement_id:
+    #             move.procurement_id.cancel()
+    #         for quant in move.quant_ids:
+    #             quant.quants_move(
+    #                 quant, move, move.location_id,
+    #                 location_from=move.location_dest_id,
+    #                 lot_id=move.lot_ids and move.lot_ids[0] or False)
+    #         move.write({'state': 'cancel', 'date': False})
+    #         if move.picking_id:
+    #             move.picking_id.action_revert_done()
+    #     return True
