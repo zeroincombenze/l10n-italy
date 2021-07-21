56d0d567a0bb8d1259b9f9c3965f079ad90db641
zeroincombenze <info@shs-av.com>
2021-07-21 07:00:40 +0200

    [FIX] l10n_it_einvoice_in with set_trace()

--- l10n_it_einvoice_in/models/partner.py
+++ l10n_it_einvoice_in/models/partner.py
@@ -293,8 +293,6 @@ class Partner(models.Model):
                         add_domain = True
                     if add_domain:
                         domain.append(constr)
-                import pdb
-                pdb.set_trace()
                 rec = self.search(domain)
                 if rec:
                     rec = rec[0]
