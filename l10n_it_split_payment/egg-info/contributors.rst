0ae9af0380f97f3dc6fb8180416d9719e30f066c
zeroincombenze <info@shs-av.com>
2022-04-27 08:17:44 +0200

    [TMP] Temporary save 202204270817

---
+++ l10n_it_split_payment/egg-info/contributors.rst
@@ -0,0 +1,4 @@
+* Davide Corio <davide.corio@abstract.it>
+* Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+* Alessio Gerace <alessio.gerace@agilebg.com>
+* Antonio Maria Vigliotti <antoniomaria.vigliotti@gmail.com>
--- l10n_it_split_payment/tests/test_splitpayment.py
+++ l10n_it_split_payment/tests/test_splitpayment.py
@@ -2,7 +2,7 @@
 # Copyright 2015  Davide Corio <davide.corio@abstract.it>
 # Copyright 2015-2016  Lorenzo Battistini - Agile Business Group
 # Copyright 2016  Alessio Gerace - Agile Business Group
-# Copyright 2018  Antonio M. Vigliotti - SHS-AV s.r.l.
+# Copyright 2018-2022  Antonio M. Vigliotti - SHS-AV s.r.l.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 #
 from odoo.tools.translate import _
