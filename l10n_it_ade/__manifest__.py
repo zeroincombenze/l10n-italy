b04ad9f58eb5b8669de6dd4850b46eae09a8ddac
zeroincombenze <info@shs-av.com>
2022-09-22 18:26:44 +0200

    [REF] l10n_it_ade/binding refactoring

--- l10n_it_ade/__manifest__.py
+++ l10n_it_ade/__manifest__.py
@@ -9,7 +9,7 @@
 #
 {
     "name": "Agenzia delle Entrate",
-    "version": "10.0.0.3.4",
+    "version": "10.0.0.3.5",
     "category": "Localization/Italy",
     "summary": "Codice e definizioni come da Agenzia delle Entrate",
     "author": "SHS-AV s.r.l.",
