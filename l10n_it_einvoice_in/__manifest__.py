9b8f921c22bb63aca9e5b3f618312fb82fdfc490
zeroincombenze <info@shs-av.com>
2022-11-15 18:11:56 +0100

    [TMP} Temporary save 20221115

--- l10n_it_einvoice_in/__manifest__.py
+++ l10n_it_einvoice_in/__manifest__.py
@@ -11,13 +11,13 @@
 #
 {
     "name": "Italian Localization - Fattura elettronica - Ricezione",
-    "version": "10.0.1.3.34",
+    "version": "10.0.1.3.35",
     "category": "Localization/Italy",
     "summary": "Ricezione fatture elettroniche",
     "author": "Agile Business Group, Innoviu, SHS-AV "
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-italy" "l10n_it_fatturapa_in",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "depends": [
         "account",
         "l10n_it_account",
@@ -33,7 +33,7 @@
         "security/ir.model.access.csv",
         "views/account_view.xml",
         "views/partner_view.xml",
-        # 'views/company_view.xml',
+        'views/company_view.xml',
         "wizard/wizard_import_fatturapa_view.xml",
         "wizard/attachment_refresh_info_view.xml",
         "wizard/link_to_existing_invoice.xml",
---
+++ l10n_it_einvoice_in/tests/data/README.txt
@@ -0,0 +1,15 @@
+XML data directory
+
+IT12345670892_00001:
+    Simple invoice, 22% VAT rate, 1 due date, 2dec, line with AC
+    RiBA
+IT10242670015_00002:
+    Professional supplier without VAT number, no VAT (N2.2), 2 due dates, 2dec
+    Credit Transfer
+IT00488410010_00014:
+    Utilityt, 4dec, round inside
+    Contains a line with negative amount
+    RiBA
+IT01641790702_00015:
+    Supplier non in test data set, 4dec, round inside
+    RiBA
