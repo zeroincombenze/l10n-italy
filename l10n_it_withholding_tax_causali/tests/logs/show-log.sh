c6fd1248a1f25358efc951c5b5e70c579e428b69
zeroincombenze <info@shs-av.com>
2023-03-25 07:16:18 +0100

    [REF] Repository refactoring

--- l10n_it_withholding_tax_causali/tests/logs/show-log.sh
+++ l10n_it_withholding_tax_causali/tests/logs/show-log.sh
@@ -1,1 +1,1 @@
-less -R $(readlink -f $(dirname $0))/l10n_it_withholding_tax_causali_20230324.txt
+less -R $(readlink -f $(dirname $0))/l10n_it_withholding_tax_causali_20230325.txt
--- l10n_it_withholding_tax_payment/tests/logs/show-log.sh
+++ l10n_it_withholding_tax_payment/tests/logs/show-log.sh
@@ -1,1 +1,1 @@
-less -R $(readlink -f $(dirname $0))/l10n_it_withholding_tax_payment_20230324.txt
+less -R $(readlink -f $(dirname $0))/l10n_it_withholding_tax_payment_20230325.txt
