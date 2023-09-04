c24a3bd84a5bc1257b7e359f91addcd4f8e748a2
zeroincombenze <info@shs-av.com>
2023-09-04 07:37:02 +0200

    [IMP] Directories egg-info renamed to readme

--- l10n_it_pec/__openerp__.py
+++ l10n_it_pec/__openerp__.py
@@ -9,15 +9,16 @@
 #    (<http://www.openerp-italia.org>).
 #
 {
-    'name': 'Italian Localization - Email PEC',
-    'version': '8.0.0.1.0',
-    'category': 'Localisation/Italy',
-    'author': 'Odoo Italia Associazione,Odoo Community Association (OCA)',
-    'website': 'http://www.openerp-italia.org',
-    'license': 'AGPL-3',
-    'depends': ['base'],
-    'data': ['views/partner_view.xml'],
-    'demo': [],
-    'test': [],
-    'installable': True,
+    "name": "Italian Localization - Email PEC",
+    "version": "8.0.0.1.0",
+    "category": "Localisation/Italy",
+    "summary": "Italian Localization - Email PEC",
+    "author": "Antonio M. Vigliotti <antoniomaria.vigliotti@gmail.com>",
+    "website": "http://www.openerp-italia.org",
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ["views/partner_view.xml"],
+    "maintainer": "Antonio M. Vigliotti <antoniomaria.vigliotti@gmail.com>",
+    "installable": True,
 }
